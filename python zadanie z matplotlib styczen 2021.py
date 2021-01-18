bdc0fdd00031cefc0f40cb9f809ae30a80787e88
jestemsobiekarol <72965102+jestemsobiekarol@users.noreply.github.com>
2021-01-18 20:51:16 +0100

    Add files via upload

---
+++ python zadanie z matplotlib styczen 2021.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 09:25:40 2021
+
+@author: karol
+"""
+import matplotlib.pyplot as pyplot
+
+print("podaj A")
+A=float(input())
+
+print("podaj d")
+d=float(input())
+
+print("podaj f")
+f=float(input())
+
+
+
+x=['1','2','3','4','5']
+y=['1','2','3','4','5']
+ 
+
+pyplot.plot(x,y)
+pyplot.xlabel("t")
+pyplot.ylabel("x(t)")
+pyplot.show()
